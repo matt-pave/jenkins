fcd6b49df4c93b5685706ca4fb12bee1da4ac1fe
Matt <matt@Matts-MacBook-Pro.local>
2021-06-10 11:58:15 +0100

    fix env

--- vars/pavePipeline.groovy
+++ vars/pavePipeline.groovy
@@ -1,10 +1,12 @@
 def call(String repoUrl) {
   pipeline {
        agent any
+       environment {
+            GIT_REPO_NAME = env.GIT_URL.replaceFirst(/^.*\/([^\/]+?).git$/, '$1')
+       }
        stages {
            stage("Tools initialization") {
                steps {
-                   env.GIT_REPO_NAME = env.GIT_URL.replaceFirst(/^.*\/([^\/]+?).git$/, '$1')
                    println env.GIT_REPO_NAME
                }
            }
