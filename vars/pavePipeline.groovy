cba07bb3f37191b5b2c10c7a9fab73a83dea01b4
Matt <matt@Matts-MacBook-Pro.local>
2021-06-29 08:45:04 +0100

    update pipeline

--- vars/pavePipeline.groovy
+++ vars/pavePipeline.groovy
@@ -60,6 +60,7 @@ step([
     $class: "GitHubCommitStatusSetter",
     reposSource: [$class: "ManuallyEnteredRepositorySource", url: env.GIT_URL],
     contextSource: [$class: "ManuallyEnteredCommitContextSource", context: "ci/jenkins/build-status"],
+    commitShaSource: [$class: "ManuallyEnteredShaSource", sha: env.GIT_COMMIT],
     errorHandlers: [[$class: "ChangingBuildStatusErrorHandler", result: "UNSTABLE"]],
     statusResultSource: [ $class: "ConditionalStatusResultSource", results: [[$class: "AnyBuildResult", message: message, state: state]] ]
 ]);
