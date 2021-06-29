ba97843bf5cc839d356bfa32ad549954b4348398
Matt <matt@Matts-MacBook-Pro.local>
2021-06-29 08:30:25 +0100

    update pipeline

--- vars/pavePipeline.groovy
+++ vars/pavePipeline.groovy
@@ -28,6 +28,16 @@ def call(String repoUrl) {
             deleteDir() /* clean up our workspace */
         }
         success {
+            void setBuildStatus(String message, String state) {
+            step([
+                $class: "GitHubCommitStatusSetter",
+                reposSource: [$class: "ManuallyEnteredRepositorySource", url: "https://github.com/my-org/my-repo"],
+                contextSource: [$class: "ManuallyEnteredCommitContextSource", context: "ci/jenkins/build-status"],
+                errorHandlers: [[$class: "ChangingBuildStatusErrorHandler", result: "UNSTABLE"]],
+                statusResultSource: [ $class: "ConditionalStatusResultSource", results: [[$class: "AnyBuildResult", message: message, state: state]] ]
+            ]);
+            }
+            setBuildStatus("Build complete", "SUCCESS");
             echo 'I succeeded!'
         }
         unstable {
